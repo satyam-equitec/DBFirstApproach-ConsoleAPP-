1f19c8c51c7e0e5c7211d3e3307cff43d5aa2484
satyam-equitec <satyam.equitec@gmail.com>
2024-02-26 11:36:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StudentDataEntities studentDataEntities = new StudentDataEntities())
+            {
+                List<Student> students = studentDataEntities.Students.ToList();
+                Console.WriteLine();
+                foreach(Student item in students)
+                {
+                    Console.WriteLine($"Name of Student: {item.FirstName}");
+                }
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
